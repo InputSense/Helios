7d514cdb32ed15bf400515b009174302c9cb9279
M B <39347854+M-B-6@users.noreply.github.com>
2023-07-27 16:07:22 -0600

    Update and rename VGTest.py to VG-template.py

---
+++ VG-template.py
@@ -0,0 +1,17 @@
+# not a necessary import
+# used for auto complete in IDEs
+from vg_helper import *
+
+
+def init():  # called first/once at runtime
+    return
+
+
+def main():
+
+    if cv_ready():
+        continue
+
+    return
+
+
