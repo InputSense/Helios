24e2519411605def26a3d199de9174c9f00e0563
Input Sense <39347854+InputSense@users.noreply.github.com>
2023-08-04 14:20:42 -0600

    Add files via upload

---
+++ buttons.py
@@ -0,0 +1,72 @@
+
+
+class buttons:
+    BUTTON_1 = 0
+    BUTTON_2 = 1
+    BUTTON_3 = 2
+    BUTTON_4 = 3
+    BUTTON_5 = 4
+    BUTTON_6 = 5
+    BUTTON_7 = 6
+    BUTTON_8 = 7
+    BUTTON_9 = 8
+    BUTTON_10 = 9
+    BUTTON_11 = 10
+    BUTTON_12 = 11
+    BUTTON_13 = 12
+    BUTTON_14 = 13
+    BUTTON_15 = 14
+    BUTTON_16 = 15
+    BUTTON_17 = 16
+    STICK_1_X = 17
+    STICK_1_Y = 18
+    STICK_2_X = 19
+    STICK_2_Y = 20
+
+
+class xb:
+    GUIDE = 0
+    MENU = 1
+    START = 2
+    RB = 3
+    RT = 4
+    RS_CLICK = 5
+    LB = 6
+    LT = 7
+    LS_CLICK = 8
+    D_UP = 9
+    D_DOWN = 10
+    D_LEFT = 11
+    D_RIGHT = 12
+    Y = 13
+    B = 14
+    A = 15
+    X = 16
+    RS_X = 17
+    RS_Y = 18
+    LS_X = 19
+    LS_Y = 20
+
+
+class ps:
+    GUIDE = 0
+    CREATE = 1
+    OPTIONS = 2
+    R1 = 3
+    R2 = 4
+    R3 = 5
+    L1 = 6
+    L2 = 7
+    L3 = 8
+    D_UP = 9
+    D_DOWN = 10
+    D_LEFT = 11
+    D_RIGHT = 12
+    TRIANGLE = 13
+    CIRCLE = 14
+    CROSS = 15
+    SQUARE = 16
+    RS_X = 17
+    RS_Y = 18
+    LS_X = 19
+    LS_Y = 20
