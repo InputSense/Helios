a193eab31a20fba1e0b51a7cac5a64df8b856c70
Input Sense <39347854+InputSense@users.noreply.github.com>
2023-08-17 14:14:36 -0600

    Update Template.py

--- Examples/CV/Template.py
+++ Examples/CV/Template.py
@@ -1,26 +1,33 @@
+# CV scripts use standard python as defined in your cv environment. Import any modules as needed
 import os
 import cv2
 
 
-
+# The main worker class, must be called CVWorker (GCVWorker also works for legacy purposes)
 class CVWorker:
 
 
     def __init__(self, width, height):
         os.chdir(os.path.dirname(__file__))
 
-        # declaring gcvdata as a class variable makes it persistent
+        # declaring gcvdata as a class variable creates it to keep persistence
         self.gcvdata = bytearray([0x00])
+        
+        # any other self variables can be declared in init
+        self.my_variable = 10
 
 
+    # it is recommended to del self variables upon stoppage
     def __del__(self):
         del self.gcvdata
 
 
     def process(self, frame):
 
-        # declaring gcvdata in frame makes it new every frame
+        # declaring gcvdata in frame creates it empty every frame
         gcvdata = bytearray([0x00])
+
+        #script logic goes here
 
         # always return frame and gcvdata
         # gcvdata must be one or the other, not both
